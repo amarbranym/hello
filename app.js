8dd39dbca22339ce50601913d6104a9f6a900ac3
AmarSinghGithub <AmarSingh94620@gmail.com>
2024-03-22 01:07:54 +0530

    port changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8700;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
